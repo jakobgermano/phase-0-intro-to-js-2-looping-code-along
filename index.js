f31bc1b9efbc8eef9e70e694ffae422e89fb735c
jakob <jakob.germano@gmail.com>
2021-03-24 19:33:09 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+// Code your solutions in this fileconst gifts = ["teddy bear", "drone", "doll"]
+function writeCards(names, event) {
+    let newArr = [];
+    for (let i = 0; i < names.length; i++) {
+        newArr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    }
+    return newArr;
+}
+
+writeCards('name', 'event');
+
+function countDown(init = 10) {
+    while (init >= 0) {
+        console.log(init--);
+    }
+}
